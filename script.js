7e388edf2a8660d64f92b059712809b68a7e8723
RuanAluno <ruan.guimaraes@aluno.senai.br>
2025-11-12 16:12:37 -0300

    alteraçao HTML E CSS

---
+++ script.js
@@ -0,0 +1,15 @@
+const apiUrl = 'http://localhost:3000/tasks';
+
+const  form = document.getElementById('task-form');
+const taskList = document.getElementById('task-list');
+
+form.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const description = document.getElementById
+('description').value;
+})
+
+
+
