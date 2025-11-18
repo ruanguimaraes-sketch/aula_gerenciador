675d2b0ed0ae639de81295b007f206a1854c0621
RuanAluno <ruan.guimaraes@aluno.senai.br>
2025-11-18 15:31:45 -0300

    FIX db.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost", 
+    host: "localhost",
     user: "root",
     password: "123456",
-    database: "db_tasks"
+    database: "db_tasks", // Troquei o nome antes era: 'tarefa_db'
 });
 
 connection.connect((err) => {
     if(err) throw err;
-    console.log("Conectado ao banco de dados MySql !");
-});
+    console.log("Conectado ao banco de dados MySql!");
+})
 
 module.exports = connection;
